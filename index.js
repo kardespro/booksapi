1f4cff3d9014ffd73d4f0de0204de9f7f9cec12a
Nego <61694826+kardespro@users.noreply.github.com>
2022-06-09 22:38:05 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const axios = require("axios")
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({status:200,message: "Super API Running"})
+});
+
+
+app.get('/api/_v1/_ping-callback', (req, res) => {
+  res.json({status:200,message: "Super API Running"})
+});
+
+app.get('/api/_v1/_books/search', async(req, res) => {
+  // super-api.oxusan.com/api/_v1/_books/search?_q=react
+  let q = req.query._q;
+  let f = req.query.filter;
+  let sort = req.query.sort;
+  let proj = req.query.projection;
+  if(!q) return res.json({status:404,message: "Query Error"})
+  let d = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${q}`)
+  let p = d.data
+  if(f){
+    let ft = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${q}&filter=${f}`)
+    res.json({
+      status:200,
+      sortType: sort || "Not Found",
+      filterType: f,
+      projection: proj ||"Not Found",
+      message: ft.data
+    })
+  }
+  if(sort){
+    let s = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${q}&orderBy=${sort}`)
+    res.json({
+      status:200,
+      sortType: sort,
+      filterType: f || "Not Found",
+      projection: proj ||"Not Found",
+      message: s.data
+    })
+  }
+  if(proj){
+    let prj = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${q}&projection=${proj}`)
+    res.json({
+      status:200,
+      sortType: sort || "Not Found",
+      filterType: f || "Not Found",
+      projection: proj,
+      message: prj.data
+    })
+  }
+  res.json({status:200,sortType: "Not Found", filterType: "Not Found",projection:"Not Found",message: p})
+});
+
+
+
+app.listen(3000, () => {
+  console.log('server started');
+});
